b2ffd1f75c122e3930e8eec2d7535261e3766b5f
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-04 23:59:52 +0530

    add login

--- main/patient/views.py
+++ main/patient/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
-from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,11 +9,14 @@ def home(request):
 #Doctor Login
 def doctor_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
         if user:
-            redirect('/')
+            login(request, user)
+            return redirect('/') 
+        else:
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'doctor/login.html', {
         'page_title': 'Doctor Login'
     })
