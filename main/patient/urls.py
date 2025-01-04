419ac58451196b18f92286a9740bf4ca30581a5c
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-04 23:21:09 +0530

    add login page

--- main/patient/urls.py
+++ main/patient/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.doctor_login, name='doctor_login'),
 ]
--- main/patient/views.py
+++ main/patient/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'doctor/home.html')
+
+#Doctor Login
+def doctor_login(request):
+    return render(request, 'doctor/login.html', {
+        'page_title': 'Doctor Login'
+    })
+
+#Reset Password
+def doctor_reset_password(request):
+    return render(request, 'doctor/reset_password.html')
