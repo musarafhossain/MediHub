7f8420ff0e089caab9c250aebd1955525a5a5467
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-21 21:11:37 +0530

    fix bugs

--- main/patient/forms.py
+++ main/patient/forms.py
@@ -62,3 +62,10 @@ class PatientForm(forms.ModelForm):
             'note': forms.Textarea(attrs={'rows': '5'}),
         }
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.CharField):
+                field.initial = 'Default value'
+            elif isinstance(field, forms.IntegerField):
+                field.initial = 0
