eb252fce54486b93b060d255452b63bae76d0bb6
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-24 22:28:19 +0530

    enable debug mode in settings; remove unused SVG icons and JavaScript files from admin static files

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pr7@a40%(q7w53s!08^6^wo4^cz1lx321$u)(u(p^86or1pex)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
