444f9ae811dfdb608bc85c10b25de528a92ce0fd
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-07-30 21:15:39 +0530

    update database settings to use MYSQL_ROOT_PASSWORD for MySQL authentication

--- main/settings.py
+++ main/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('MYSQL_DATABASE', 'medihub'),
         'USER': os.getenv('MYSQL_USER', 'root'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD', ''),
+        'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD', ''),
         'HOST': os.getenv('MYSQL_HOST', 'localhost'),
         'PORT': os.getenv('MYSQL_PORT', '3306'),
         'OPTIONS': {
